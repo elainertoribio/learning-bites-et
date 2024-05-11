52ba0b22a3382466fa0fa185a14403f653207f89
elainertoribio <toribioelainer@gmail.com>
2024-05-11 16:51:29 -0400

    initial push

---
+++ force-app/main/default/lwc/exercise1/exercise1.js
@@ -0,0 +1,71 @@
+import { LightningElement,track } from 'lwc';
+
+export default class Exercise1 extends LightningElement {
+
+    displayInformation = false;
+    hideDrivers = true;
+    selectedDriver = {};
+
+    @track
+    drivers = [{
+        Id: 1,
+        Name: 'Lewis Hamilton',
+        Team: 'Mercedes',
+        Age: 30,
+        RaceWins: 103,
+        IsRaceWinner: true
+    },
+    {
+        Id: 2,
+        Name: 'Lando Norris',
+        Team: 'McLaren',
+        Age: 21,
+        RaceWins: 1,
+        IsRaceWinner: true
+    },
+    {
+        Id: 3,
+        Name: 'Max Verstappen',
+        Team: 'Red Bull',
+        Age: 22,
+        RaceWins: 58,
+        IsRaceWinner: true
+    },
+    {
+        Id: 4,
+        Name: 'Charles Leclerc',
+        Team: 'Ferrari',
+        Age: 23,
+        RaceWins: 5,
+        IsRaceWinner: true
+    },
+    {
+        Id: 5,
+        Name: 'Oscar Piastri',
+        Team: 'McLaren',
+        Age: 20,
+        RaceWins: 0,
+        IsRaceWinner: false
+    },
+    {
+        Id: 6,
+        Name: 'Yuki Tsunoda',
+        Team: 'RB VCard',
+        Age: 21,
+        RaceWins: 0,
+        IsRaceWinner: false
+    }
+    ];
+
+    handleClick(evt) {
+        const driverId = evt.target.dataset.id;
+        this.selectedDriver = this.drivers.find(driver => driver.Id == driverId);
+        this.selectedDriver ? this.displayInformation = true : this.displayInformation = false;
+        this.selectedDriver ? this.hideDrivers = false : this.hideDrivers = true;
+        setTimeout(() => {
+            this.displayInformation = false;
+            this.hideDrivers = true;
+        }, 2000);
+
+    }
+}
