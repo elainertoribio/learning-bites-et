f5ca37709692ec81669eed722f778f9c038fe87d
elainertoribio <toribioelainer@gmail.com>
2024-05-13 09:10:33 -0400

    renaming

--- force-app/main/default/lwc/lwcBasics/lwcBasics.js
+++ force-app/main/default/lwc/lwcBasics/lwcBasics.js
@@ -1,6 +1,6 @@
 import { LightningElement,track } from 'lwc';
 
-export default class Exercise1 extends LightningElement {
+export default class LwcBasics extends LightningElement {
 
     displayInformation = false;
     hideDrivers = true;
